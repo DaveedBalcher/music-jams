250dd73bf75833f9ac9c5ef662402acc39e20a83
David Balcher <David.Balcher@gmail.com>
2022-09-02 15:46:27 -0400

    Moved extension on Mapkit to separate file

--- PhillyJams/VenuesViewModel.swift
+++ PhillyJams/VenuesViewModel.swift
@@ -94,12 +94,3 @@ extension Coordinates {
     }
 }
 
-extension MKCoordinateRegion: Equatable {
-    
-    public static func == (lhs: MKCoordinateRegion, rhs: MKCoordinateRegion) -> Bool {
-        lhs.center.latitude == rhs.center.latitude &&
-        lhs.center.longitude == rhs.center.longitude &&
-        lhs.span.latitudeDelta == rhs.span.latitudeDelta &&
-        lhs.span.longitudeDelta == rhs.span.longitudeDelta
-    }
-}
