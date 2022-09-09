959ac8362f4b88d790b3e1842cbfa1bb2fdbc0c7
David Balcher <David.Balcher@gmail.com>
2022-09-09 13:40:40 -0400

    Added CSVVenueLoader

---
+++ MusicVenues/VenueAPI/CSVVenueLoader.swift
@@ -0,0 +1,15 @@
+//
+//  CSVVenueLoader.swift
+//  MusicVenues
+//
+//  Created by Daveed Balcher on 9/9/22.
+//
+
+import Foundation
+
+public final class CSVVenueLoader: VenueLoader {
+    
+    public func retrieveVenues() -> [VenueItem] {
+        return []
+    }
+}
