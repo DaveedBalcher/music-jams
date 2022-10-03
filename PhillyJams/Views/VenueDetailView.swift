6bbc5a2bf677a5af4987bff8c5b4cd736bdd9825
David Balcher <David.Balcher@gmail.com>
2022-10-03 12:37:49 -0600

    Add Color to VenueDetail NavBar

--- PhillyJams/Views/VenueDetailView.swift
+++ PhillyJams/Views/VenueDetailView.swift
@@ -10,91 +10,99 @@ import MusicVenues
 
 struct VenueDetailView: View {
     let venue: VenueItem
-
+    
     @State private var isPresentingWebView = false
     
     var body: some View {
         
-        let neightborhoodTint = Color(venue.neighborhood?.color, defaultColor: .black)
-        
-        ScrollView {
-            VStack {
-                ZStack(alignment: .bottomLeading) {
-                    Image(systemName: "music.note")
-                        .resizable()
-                        .padding(90)
+        ZStack(alignment: .top) {
+            Rectangle()
+                .fill(.regularMaterial)
+                .frame(maxHeight: 92)
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack {
+                    ZStack(alignment: .bottomLeading) {
+                        let neightborhoodTint = Color(venue.neighborhood?.color, defaultColor: .black)
+                        
+                        Image(systemName: "music.note")
+                            .resizable()
+                            .padding(90)
+                            .foregroundColor(.white)
+                            .scaledToFit()
+                            .background(
+                                Rectangle()
+                                    .foregroundColor(neightborhoodTint)
+                                    .cornerRadius(8)
+                                    .shadow(radius: 8)
+                            )
+                        
+                        VStack(alignment: .leading) {
+                            Text(venue.neighborhood?.name ?? "")
+                                .font(.title3)
+                                .fontWeight(.light)
+                            Text("Genres: \(venue.genresDescription)")
+                                .fontWeight(.light)
+                            Text("Vibe: \(venue.vibe)")
+                                .fontWeight(.light)
+                        }
+                        .font(.subheadline)
                         .foregroundColor(.white)
-                        .scaledToFit()
-                        .background(
-                            Rectangle()
-                                .foregroundColor(neightborhoodTint)
-                                .cornerRadius(8)
-                                .shadow(radius: 8)
-                        )
-                    
-                    VStack(alignment: .leading) {
-                        Text(venue.neighborhood?.name ?? "")
-                            .font(.title3)
-                            .fontWeight(.light)
-                        Text("Genres: \(venue.genresDescription)")
-                            .fontWeight(.light)
-                        Text("Vibe: \(venue.vibe)")
-                            .fontWeight(.light)
+                        .padding(12)
                     }
-                    .font(.subheadline)
-                    .foregroundColor(.white)
                     .padding(12)
-                }
-                .padding(12)
-
-
-                // TODO: Add venue description
-                
-                Text("Events")
-                    .font(.headline)
-                    .padding()
-                
-                if venue.events.isEmpty {
-                                    Text("List of jams and open mics coming soon...")
-                                        .padding()
-                                        .font(.callout)
-                                        .foregroundStyle(.gray)
-                } else {
-                    ForEach(venue.events) { event in
-                        HStack {
-                            let title = [event.type, event.name].filter { !$0.isEmpty }.joined(separator: ": ")
-                            let date = [event.dayOfTheWeek, event.startTime, event.endTime].filter { !$0.isEmpty }.joined(separator: " - ")
-                            let hosts = event.hosts?.filter { !$0.isEmpty }.joined(separator: " and ")
-                            
-                            VStack(alignment: .leading) {
-                                Text(title)
-                                    .fontWeight(.semibold)
-                                Text("Host(s): \(hosts ?? "unspecified")")
-                                    .fontWeight(.light)
-                                Text("Every \(date)")
-                                    .fontWeight(.light)
-                                if let url = URL(string: event.url ?? "") {
-                                    Button {
-                                        isPresentingWebView.toggle()
-                                    } label: {
-                                        Text("Visit Event Page")
-                                            .foregroundColor(Color.lightBlue)
-                                    }
-                                    .sheet(isPresented: $isPresentingWebView) {
-                                        WebView(url: url)
+                    
+                    
+                    // TODO: Add venue description
+                    
+                    Text("Events")
+                        .font(.headline)
+                        .padding()
+                    
+                    if venue.events.isEmpty {
+                        Text("List of jams and open mics coming soon...")
+                            .padding()
+                            .font(.callout)
+                            .foregroundStyle(.gray)
+                    } else {
+                        ForEach(venue.events) { event in
+                            HStack {
+                                let title = [event.type, event.name].filter { !$0.isEmpty }.joined(separator: ": ")
+                                let date = [event.dayOfTheWeek, event.startTime, event.endTime].filter { !$0.isEmpty }.joined(separator: " - ")
+                                let hosts = event.hosts?.filter { !$0.isEmpty }.joined(separator: " and ")
+                                
+                                VStack(alignment: .leading) {
+                                    Text(title)
+                                        .fontWeight(.semibold)
+                                    Text("Host(s): \(hosts ?? "unspecified")")
+                                        .fontWeight(.light)
+                                    Text("Every \(date)")
+                                        .fontWeight(.light)
+                                    if let url = URL(string: event.url ?? "") {
+                                        Button {
+                                            isPresentingWebView.toggle()
+                                        } label: {
+                                            Text("Visit Event Page")
+                                                .foregroundColor(Color.lightBlue)
+                                        }
+                                        .sheet(isPresented: $isPresentingWebView) {
+                                            WebView(url: url)
+                                        }
                                     }
                                 }
+                                .font(.subheadline)
+                                .padding(8)
+                                
+                                Spacer()
                             }
-                            .font(.subheadline)
-                            .padding(8)
-                            
-                            Spacer()
+                            .padding([.leading, .trailing], 16)
                         }
-                        .padding([.leading, .trailing], 16)
                     }
+                    
                 }
             }
-            .toolbar {                
+            .toolbar {
                 ToolbarItem(placement: .primaryAction) {
                     Link(destination: URL(string: "https://forms.gle/ZdcBWFYL97iuhDZx8")!) {
                         Label {
@@ -117,6 +125,8 @@ struct VenueDetailView: View {
 
 struct VenueDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        VenueDetailView(venue: VenueItem.defaultItem)
+        NavigationView {
+            VenueDetailView(venue: VenueItem.defaultItem)
+        }
     }
 }
