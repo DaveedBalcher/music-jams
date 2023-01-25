21a8e348d5a1016af049677c2ece5dbdfef78fe3
David Balcher <David.Balcher@gmail.com>
2023-01-25 10:36:22 -0500

    Project File structure clean up to show hierarchy of views

---
+++ PhillyJams/Views/ContainerView+Subviews/InfoBannerView.swift
@@ -0,0 +1,49 @@
+//
+//  InfoBannerView.swift
+//  PhillyJams
+//
+//  Created by Daveed Balcher on 11/11/22.
+//
+
+import SwiftUI
+
+struct InfoBannerView: View {
+    @Binding var isPresentingInfo: Bool
+    let backgroundColor: Color = .white
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .foregroundColor(backgroundColor)
+
+            HStack {
+                Link("Add an Event", destination: URL(string: "https://forms.gle/ZdcBWFYL97iuhDZx8")!)
+                    .font(.subheadline)
+                    .foregroundColor(Color.lightBlue)
+                
+                Text("|")
+                    .font(.system(size: 16))
+                    .foregroundColor(.black.opacity(0.25))
+                    .padding([.bottom],4)
+                
+                Button {
+                    isPresentingInfo = true
+                } label: {
+                    Label {
+                        Text("")
+                    } icon: {
+                        Image(systemName: "info.circle")
+                            .font(.system(size: 16))
+                            .foregroundColor(.lightBlue)
+                    }
+                }
+                .padding([.leading], -8)
+            }
+            .padding([.leading], 4)
+            .padding([.trailing], 8)
+            
+        }
+        .frame(maxHeight: 24)
+        .padding([.trailing], -4)
+    }
+}
